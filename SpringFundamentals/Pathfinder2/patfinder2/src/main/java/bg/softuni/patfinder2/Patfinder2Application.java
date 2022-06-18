badae8969241ddcc742508987114051c85bccbed
Valentin <valentin_stojanov@abv.bg>
2022-06-18 16:36:49 +0300

    Java-Web\SpringFundamentals - initial commit of Pathfinder2.

---
+++ SpringFundamentals/Pathfinder2/patfinder2/src/main/java/bg/softuni/patfinder2/Patfinder2Application.java
@@ -0,0 +1,13 @@
+package bg.softuni.patfinder2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Patfinder2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Patfinder2Application.class, args);
+    }
+
+}
---
+++ SpringFundamentals/Pathfinder2/patfinder2/src/test/java/bg/softuni/patfinder2/Patfinder2ApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.patfinder2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Patfinder2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
