38452c93a59d56c97620c2b3c9916eedcac9cfdc
Valentin <valentin_stojanov@abv.bg>
2022-07-20 23:19:56 +0300

    Java-Web\SpringFundamentals\mobilele - working on page translation.

--- SpringFundamentals/mobilele/src/main/java/bg/softuni/mobilele/config/I18Config.java
+++ SpringFundamentals/mobilele/src/main/java/bg/softuni/mobilele/config/I18Config.java
@@ -29,7 +29,7 @@ public class I18Config {
     public MessageSource messageSource(){
         ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
         resourceBundleMessageSource.setBasename("i18n/messages");
-
+        resourceBundleMessageSource.setDefaultEncoding("UTF-8");
         return  resourceBundleMessageSource;
     }
 }
