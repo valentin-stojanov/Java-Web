3c4130ceef698690b5ace534c400b8f26b82a906
valentin-stojanov <valentin_stojanov@abv.bg>
2022-07-25 14:45:40 +0300

    Java-Web\SpringFundamentals\pathfinder2 - added routes index page..

--- SpringFundamentals/Pathfinder2/patfinder2/src/main/java/bg/softuni/patfinder2/config/PathfinderSecurityConfiguration.java
+++ SpringFundamentals/Pathfinder2/patfinder2/src/main/java/bg/softuni/patfinder2/config/PathfinderSecurityConfiguration.java
@@ -14,6 +14,14 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class PathfinderSecurityConfiguration {
 
+    // 1. add spring security starter dependency
+// 2. Define the following beans
+//    a. SecurityFilterChain -> here we describe authnetication rules, how to perform login/logout etc.
+//    b. PasswordEncoder -> used when saving a User in the DB to hash the password,
+//                          spring security uses it to validate password when logging
+//    c. UserDetailsService -> shows Spring how to fetch the user from the db, wraps the User entity into a UserDetails interface impl
+// 3. add thymeleaf security to hide restricted elements
+// 4. use Principal object (which we get as an argument in the controller methods)
     private final UserRepository userRepository;
 
     public PathfinderSecurityConfiguration(UserRepository userRepository) {
@@ -34,7 +42,7 @@ public class PathfinderSecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                .antMatchers("/").permitAll()
+                .antMatchers("/", "/routes/**").permitAll()
                 .antMatchers( "/users/login", "/users/register").anonymous()
                 .antMatchers( "/users/profile").authenticated()
                 .and()
